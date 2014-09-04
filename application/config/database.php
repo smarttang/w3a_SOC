feb92c562e1ef8ca1bf409ac2fb4bac87d086f58
smarttang <tangyucong@163.com>
2014-09-04 19:24:30 +0800

    更新BUG

--- application/config/database.php
+++ application/config/database.php
@@ -48,7 +48,7 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
+$db['default']['hostname'] = '127.0.0.1';
 $db['default']['username'] = 'root';
 $db['default']['password'] = '123456';
 $db['default']['database'] = 'w3a_system';
