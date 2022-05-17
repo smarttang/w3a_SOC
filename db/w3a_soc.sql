dbf6c0943a9d3b34d91b4cf43178539870ac4932
smarttang <tangyucong@163.com>
2022-05-18 01:07:49 +0800

    更新数据结构还有样例数据

--- db/w3a_soc.sql
+++ db/w3a_soc.sql
@@ -17,16 +17,13 @@ USE w3a_soc;
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
--- ----------------------------
--- Table structure for w3_alter_channel
--- ----------------------------
 DROP TABLE IF EXISTS `w3_alter_channel`;
 CREATE TABLE `w3_alter_channel` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
   `alter_id` bigint(20) NOT NULL COMMENT 'id',
   `alter_source_id` bigint(20) NOT NULL COMMENT '告警渠道id',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COMMENT='告警渠道表';
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='告警渠道表';
 
 -- ----------------------------
 -- Table structure for w3_alter_config
@@ -44,7 +41,7 @@ CREATE TABLE `w3_alter_config` (
   `alter_config_token` varchar(255) DEFAULT '' COMMENT '企业微信、钉钉的TOKEN',
   `alter_config_delete` tinyint(1) DEFAULT '1' COMMENT '逻辑删除,0：已删除，1：启用',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='告警配置表';
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='告警配置表';
 
 -- ----------------------------
 -- Table structure for w3_alter_logs
@@ -56,7 +53,7 @@ CREATE TABLE `w3_alter_logs` (
   `alter_createtime` datetime NOT NULL COMMENT '创建时间',
   `alter_info` longtext NOT NULL COMMENT '告警内容',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='告警记录表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='告警记录表';
 
 -- ----------------------------
 -- Table structure for w3_alter_master
@@ -72,7 +69,7 @@ CREATE TABLE `w3_alter_master` (
   `alter_status` tinyint(1) DEFAULT '1' COMMENT '告警状态,0：关闭，1：开启',
   `alter_delete` tinyint(1) DEFAULT '1' COMMENT '告警状态,0：删除，1：启用',
   PRIMARY KEY (`alter_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COMMENT='告警主表';
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='告警主表';
 
 -- ----------------------------
 -- Table structure for w3_alter_paramets
@@ -89,6 +86,17 @@ CREATE TABLE `w3_alter_paramets` (
   `alter_time` tinyint(1) DEFAULT '1' COMMENT '时间类型,1：每分钟，2：每小时,3:每天,4:每周',
   PRIMARY KEY (`alter_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='告警参数表';
+
+-- ----------------------------
+-- Table structure for w3_license
+-- ----------------------------
+DROP TABLE IF EXISTS `w3_license`;
+CREATE TABLE `w3_license` (
+  `id` tinyint(1) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `license_type` tinyint(1) DEFAULT '1' COMMENT '类型,1:License版本,2:License内容',
+  `lincese_info` char(32) DEFAULT '' COMMENT 'License内容',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='License管理';
 
 -- ----------------------------
 -- Table structure for w3_statistics
@@ -115,7 +123,7 @@ CREATE TABLE `w3_users` (
   `user_status` tinyint(1) DEFAULT '1' COMMENT '规则状态,0：禁用，1：启用',
   `user_delete` tinyint(1) DEFAULT '1' COMMENT '逻辑删除状态,0：已删除，1：启用',
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COMMENT='用户表';
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8 COMMENT='用户表';
 
 -- ----------------------------
 -- Table structure for w3_vuls_circulation
@@ -125,10 +133,10 @@ CREATE TABLE `w3_vuls_circulation` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
   `vid` bigint(20) NOT NULL COMMENT 'id',
   `vul_circulation_createtime` datetime NOT NULL COMMENT '流转时间',
-  `vul_dispose_status` tinyint(1) DEFAULT '0' COMMENT '处置状态,0:待处理,1:解决中,2:误报忽略,3:已解决,4:白名单',
+  `vul_dispose_status` tinyint(1) DEFAULT '0' COMMENT '处置状态,0:待处理,1:解决中,2:误报忽略,3:已解决,4:白名单,5:无效单,6:修改',
   `vul_circulation_info` varchar(255) DEFAULT '暂无' COMMENT '流转信息',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='漏洞管理.流转单';
+) ENGINE=InnoDB AUTO_INCREMENT=74 DEFAULT CHARSET=utf8 COMMENT='漏洞管理.流转单';
 
 -- ----------------------------
 -- Table structure for w3_vuls_service
@@ -141,17 +149,21 @@ CREATE TABLE `w3_vuls_service` (
   `vul_createtime` datetime NOT NULL COMMENT '创建时间',
   `vul_updatetime` datetime DEFAULT NULL COMMENT '更新时间',
   `vul_solvetime` datetime DEFAULT NULL COMMENT '解决时间',
-  `vul_info` varchar(255) NOT NULL COMMENT '漏洞信息',
-  `vul_suggestion` varchar(255) DEFAULT '暂无' COMMENT '解决建议',
-  `vul_referer` varchar(255) DEFAULT '暂无' COMMENT '引用参考',
-  `vul_level` tinyint(1) DEFAULT '0' COMMENT '漏洞级别',
+  `vul_info` longtext NOT NULL COMMENT '漏洞信息',
+  `vul_suggestion` longtext COMMENT '解决建议',
+  `vul_referer` longtext COMMENT '引用参考',
+  `vul_level` tinyint(1) DEFAULT '0' COMMENT '漏洞级别,0:低危,1:中危,2:高危',
   `vul_source` tinyint(1) DEFAULT '0' COMMENT '漏洞来源,0:内部,1:第三方',
   `vul_source_object` tinyint(1) DEFAULT '0' COMMENT '漏洞来源对象,0:人工,1:工具检测,2:公司名,3:白帽子',
   `vul_source_info` varchar(255) DEFAULT NULL COMMENT '漏洞来源对象内容',
   `vul_dispose_status` tinyint(1) DEFAULT '0' COMMENT '处置状态,0:待处理,1:解决中,2:误报忽略,3:已解决,4:白名单',
   `vul_circulation` tinyint(1) DEFAULT '0' COMMENT '内部流转,0:未接入,1:接入',
+  `vul_delete` tinyint(1) DEFAULT '1' COMMENT '逻辑删除,0:已删除,1:正常',
+  `vul_tags` varchar(255) DEFAULT '' COMMENT '漏洞标签,用,号分开',
+  `vul_uuid` bigint(20) DEFAULT '0' COMMENT '漏洞创建人，如果是工具，默认为0',
+  `vul_hosts` bigint(20) NOT NULL COMMENT '漏洞归属站点的ID',
   PRIMARY KEY (`id`,`vul_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='漏洞管理';
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COMMENT='漏洞管理';
 
 -- ----------------------------
 -- Table structure for w3_vuls_tags
@@ -159,10 +171,9 @@ CREATE TABLE `w3_vuls_service` (
 DROP TABLE IF EXISTS `w3_vuls_tags`;
 CREATE TABLE `w3_vuls_tags` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
-  `vid` bigint(20) NOT NULL COMMENT 'id',
   `vul_tags` varchar(30) NOT NULL COMMENT '漏洞标签',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='漏洞管理.风险标签';
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COMMENT='漏洞管理.风险标签';
 
 -- ----------------------------
 -- Table structure for w3_web_attack_rules
@@ -179,7 +190,7 @@ CREATE TABLE `w3_web_attack_rules` (
   `rules_delete` tinyint(1) DEFAULT '1' COMMENT '逻辑删除状态,0：已删除，1：启用',
   `rules_regex` longtext NOT NULL COMMENT '规则内容',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=utf8 COMMENT='web检测规则';
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COMMENT='web检测规则';
 
 -- ----------------------------
 -- Table structure for w3_web_attacks
@@ -204,7 +215,7 @@ CREATE TABLE `w3_web_attacks` (
   `attack_area` varchar(10) DEFAULT '未知' COMMENT '归属地区',
   `attack_judge` varchar(255) DEFAULT '暂无' COMMENT '判定备注',
   PRIMARY KEY (`attack_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8 COMMENT='攻击日志信息';
+) ENGINE=InnoDB AUTO_INCREMENT=5539 DEFAULT CHARSET=utf8 COMMENT='攻击日志信息';
 
 -- ----------------------------
 -- Table structure for w3_web_attacks_circulation
@@ -216,7 +227,7 @@ CREATE TABLE `w3_web_attacks_circulation` (
   `attack_circulation_createtime` datetime NOT NULL COMMENT '流转时间',
   `attack_circulation_logs` varchar(255) DEFAULT '暂无' COMMENT '流转信息',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8 COMMENT='攻击日志信息.流转单';
+) ENGINE=InnoDB AUTO_INCREMENT=5549 DEFAULT CHARSET=utf8 COMMENT='攻击日志信息.流转单';
 
 -- ----------------------------
 -- Table structure for w3_website
@@ -231,7 +242,7 @@ CREATE TABLE `w3_website` (
   `website_updatetime` datetime DEFAULT NULL COMMENT '更新时间',
   `website_delete` tinyint(1) DEFAULT '1' COMMENT '逻辑删除,0:删除，1:可用',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8 COMMENT='站点管理';
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='站点管理';
 
 -- ----------------------------
 -- Table structure for w3_website_config
@@ -260,7 +271,6 @@ CREATE TABLE `w3_website_service` (
   `website_updatetime` datetime DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`,`website_types`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='站点管理.服务配置';
-
 
 /**
  * 初始化数据
